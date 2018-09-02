506374181deaa74744ec8a182d1568a1085c588c
Murad Hasan <mamun.me4u@gmail.com>
2018-09-03 01:25:26 +0600

    separeate two user and admin

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+/* Route::get('/product/{id}',[
+
+	'uses'=>'FrontEndController@singleProduct',
+	'as'=>'product.single'
+
+]); */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('admin/home','AdminController@index');
+Route::get('admin','Admin\LoginController@showLoginForm')->name('admin.login');
+Route::post('admin','Admin\LoginController@login');
+Route::post('admin-password/email','ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::get('admin-password/reset','ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('admin-password/reset','ResetPasswordController@reset');
+Route::get('admin-password/reset/{token}','ResetPasswordController@showResetForm')->name('admin.password.reset');
+
